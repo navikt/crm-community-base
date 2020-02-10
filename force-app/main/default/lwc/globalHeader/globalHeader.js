b76a043d7c1e6338affc78ebb4ddeaf4195c52bd
Frode Hoen <frode.hoen@soprasteria.com>
2020-02-10 08:59:18 +0100

    Only main navigation bar

--- force-app/main/default/lwc/globalHeader/globalHeader.js
+++ force-app/main/default/lwc/globalHeader/globalHeader.js
@@ -1,5 +1,9 @@
 import { LightningElement } from 'lwc';
+import IDPORTEN_IKON from '@salesforce/resourceUrl/idporten_ikon';
+import LOGO from '@salesforce/resourceUrl/logo';
 
 export default class GlobalHeader extends LightningElement {
 
+    logo=LOGO;
+    idportenIkon=IDPORTEN_IKON;
 }
