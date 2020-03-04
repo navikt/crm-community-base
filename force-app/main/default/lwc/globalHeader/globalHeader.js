e63f7d329e15c0aa6b50a32f4a607c33fb559816
Frode Hoen <frode.hoen@soprasteria.com>
2020-03-04 09:05:47 +0100

    Added search button

--- force-app/main/default/lwc/globalHeader/globalHeader.js
+++ force-app/main/default/lwc/globalHeader/globalHeader.js
@@ -1,9 +1,11 @@
 import { LightningElement } from 'lwc';
 import IDPORTEN_IKON from '@salesforce/resourceUrl/idporten_ikon';
 import LOGO from '@salesforce/resourceUrl/logo';
+import SEARCH_BUTTON from '@salesforce/resourceUrl/sok_liten_morkeblaa';
 
 export default class GlobalHeader extends LightningElement {
 
     logo=LOGO;
     idportenIkon=IDPORTEN_IKON;
+    searchButton=SEARCH_BUTTON;
 }
