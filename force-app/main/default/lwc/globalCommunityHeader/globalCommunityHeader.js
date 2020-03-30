f5e50c46d057f7a12257512b686993337d27b466
vmjelde <vegard.mjelde@soprasteria.com>
2020-03-30 15:56:06 +0200

    Header and Footer adapts to screen size
    https://jira.adeo.no/browse/TOLK-164

--- force-app/main/default/lwc/globalCommunityHeader/globalCommunityHeader.js
+++ force-app/main/default/lwc/globalCommunityHeader/globalCommunityHeader.js
@@ -5,19 +5,30 @@ import CURRENT_USER from '@salesforce/schema/User.Name';
 import IDPORTEN_IKON from '@salesforce/resourceUrl/idporten_ikon';
 import LOGO from '@salesforce/resourceUrl/logo';
 import SEARCH_BUTTON from '@salesforce/resourceUrl/sok_liten_morkeblaa';
+import HAMBURGER_MENY from '@salesforce/resourceUrl/hamburger_meny_ikon';
 
 const fields = [CURRENT_USER];
 
 export default class GlobalHeader extends LightningElement {
-    userId = Id;
+	userId = Id;
 
-        @wire(getRecord, { recordId: '$userId', fields })
-        user;
+	@wire(getRecord, { recordId: '$userId', fields })
+	user;
 
-        get currentUser() {
-            return getFieldValue(this.user.data, CURRENT_USER);
-        }
-    logo=LOGO;
-    idportenIkon=IDPORTEN_IKON;
-    searchButton=SEARCH_BUTTON;
+	get currentUser() {
+		return getFieldValue(this.user.data, CURRENT_USER);
+	}
+	logo = LOGO;
+	idportenIkon = IDPORTEN_IKON;
+	searchButton = SEARCH_BUTTON;
+	hamburgerMenyIkon = HAMBURGER_MENY;
+
+	toggleMobileMenu() {
+		var x = this.template.querySelector(".accessible-megamenu");
+		if (x.classList.contains('m-open')) {
+			x.classList.remove('m-open');
+		} else {
+			x.classList.add('m-open');
+		}
+	}
 }
