cd6befd32c1fcffe53838cfd6db9fdeb6fff4b85
vmjelde <vegard.mjelde@soprasteria.com>
2020-05-16 11:49:43 +0200

    Creating new header and footer
    https://jira.adeo.no/browse/TOLK-376

--- force-app/main/default/lwc/globalCommunityHeaderNew/globalCommunityHeaderNew.js
+++ force-app/main/default/lwc/globalCommunityHeaderNew/globalCommunityHeaderNew.js
@@ -1,6 +1,41 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
+import ID from '@salesforce/user/Id';
+import CURRENT_USER from '@salesforce/schema/User.Name';
+
+const fields = [CURRENT_USER];
+
+var prevScrolled = 0;
+const headerHeight = 91;
+const headerStartPosition = 44;
+var headerPosition = headerStartPosition;
+
+window.addEventListener('scroll', () => {
+    var scrolled = window.scrollY;
+    const difference = scrolled - prevScrolled;
+    if(headerPosition > -headerHeight){
+        headerPosition = headerPosition - difference;
+    }
+    else if(difference < 0){
+        headerPosition = headerPosition - difference;
+    }
+    //console.log(headerPosition);
+    prevScrolled = scrolled;
+
+});
 
 export default class GlobalCommunityHeaderNew extends LightningElement {
+
+
+    userId = ID;
+	@wire(getRecord, { recordId: '$userId', fields })
+    user;
+	get currentUser() {
+		return getFieldValue(this.user.data, CURRENT_USER);
+    }
+    
 
     handleOnScroll(){
     }
@@ -9,48 +44,58 @@ export default class GlobalCommunityHeaderNew extends LightningElement {
     @track searchPressed = false;
     handleOnClickSearch(event){
         this.searchPressed = !this.searchPressed;
-        console.log(this.searchPressed);
     }
-    @track searchInput;
-    @track newPage ="";
-    handleSearching(event){
-        this.searchInput = event.target.value;
-        var formatedSearch = this.searchInput.split(" ").join("%20");
-        this.newPage = 'https://www.nav.no/sok\?ord=' + formatedSearch;
-        console.log(this.newPage);
-        this.loadSearchResults();
+    @track varslerPressed = false;
+    onHandleClickVarsler(){
+        this.varslerPressed = !this.varslerPressed;
     }
-    loadSearchResults(){
-        console.log("loadSearchResults");
-    }
-    handleSearch(){
-        return this.newPage;
-    
+    @track minSidePressed = false;
+    handleOnClickMinSide(){
+        this.minSidePressed = !this.minSidePressed;
     }
 
 
     @track menuPressed=false;
     handleOnClickMenu(event){
         this.menuPressed=!this.menuPressed;
+        this.sendMenuSelectedEvent();
     }
+
     @track isPrivatPerson=true;
     @track isArbeidsgiver=false;
     @track isSamarbeidspartner=false;
-    handleOnClickPrivatPerson(){
+    handleOnClickPrivatPerson(event){
         this.isPrivatPerson=true;
         this.isArbeidsgiver=false;
         this.isSamarbeidspartner=false;
+        this.sendClientTypeSelectedEvent();  
     }
-    handleOnClickArbeidsgiver(){
+    handleOnClickArbeidsgiver(event){
         this.isPrivatPerson=false;
         this.isArbeidsgiver=true;
         this.isSamarbeidspartner=false;
+        this.sendClientTypeSelectedEvent();
     }
-    handleOnClickSamarbeidspartner(){
+    handleOnClickSamarbeidspartner(event){
         this.isPrivatPerson=false;
         this.isArbeidsgiver=false;
         this.isSamarbeidspartner=true;
+        this.sendClientTypeSelectedEvent();
     }
+
+    @wire(CurrentPageReference) pageRef;
+    sendClientTypeSelectedEvent(){
+        var data = {
+            isPrivatPerson : this.isPrivatPerson,
+            isArbeidsgiver : this.isArbeidsgiver,
+            isSamarbeidspartner : this.isSamarbeidspartner,
+        }
+        fireEvent(this.pageRef, 'clienttypeselected', data);
+    }
+    sendMenuSelectedEvent(){
+        fireEvent(this.pageRef, 'menuSelectedEvent', this.menuPressed);
+    }
+   
 
     
 }
