41131876fc85eea7aac484b80c9a5ea3ecddd136
Frode Hoen <frode.hoen@soprasteria.com>
2020-06-24 08:46:22 +0200

    Menu for english and sami

---
+++ force-app/main/default/lwc/footerEnglish/footerEnglish.js
@@ -0,0 +1,63 @@
+import { LightningElement, track, wire, api } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
+import isProdFunction from '@salesforce/apex/GlobalCommunityHeaderFooterController.isProd';
+
+export default class GlobalCommunityFooter extends LightningElement {
+
+	@api NAVarea;
+
+	//@track isProd = window.location.toString().includes("tolkebestilling.nav.no/");
+	@track isPrivatPerson = true;
+	@track isProd;
+	@track error;
+	@wire(isProdFunction)
+	wiredIsProd({ error, data }) {
+		this.isProd = data;
+		console.log("isProd: " + this.isProd);
+	}
+
+	scrollToTop() {
+		window.scroll(0, 0, 'smooth');
+		
+	}
+
+	@track isDelSkjerm = false;
+	onHandleClickDelSkjerm() {
+		this.isDelSkjerm = !this.isDelSkjerm;
+	}
+
+	@track isSkjermdelingLesMer = false;
+	onHandleClickSkjermdelingInfo() {
+		this.isSkjermdelingLesMer = !this.isSkjermdelingLesMer;
+	}
+
+	functionalityNotSupported() {
+		alert("Vi støtter dessverre ikke denne funksjonaliteten i dag.");
+	}
+
+	@wire(CurrentPageReference) pageRef;
+	connectedCallback() {
+		registerListener('clienttypeselected', this.handleClientTypeSelected, this);
+		registerListener('menuSelectedEvent', this.handleMenuSelected, this);
+		//this.isPrivatPerson= this.NAVarea == 'Privatperson';
+	}
+	disconnectedCallback() {
+		unregisterAllListeners(this);
+	}
+
+	//@track isPrivatPerson = true;
+	@track isArbeidsgiver = false;
+	@track isSamarbeidspartner = false;
+	handleClientTypeSelected(data) {
+		this.isPrivatPerson = data.isPrivatPerson;
+		this.isArbeidsgiver = data.isArbeidsgiver;
+		this.isSamarbeidspartner = data.isSamarbeidspartner;
+	}
+
+	@track menuPressed = false;
+	handleMenuSelected(isSelected) {
+		this.menuPressed = isSelected;
+	}
+
+}
---
+++ force-app/main/default/lwc/headerSami/headerSami.js
@@ -0,0 +1,61 @@
+import { LightningElement, track, wire, api } from 'lwc';
+
+const screenWidth = screen.width;
+const headerHeight = screenWidth > 576 ? 91 : 88;
+const headerStartPosition = screenWidth > 576 ? 44 : 0;
+var headerPosition = headerStartPosition;
+var hovedbannerposition = headerStartPosition + headerHeight;
+document.documentElement.style.setProperty('--fixed', "fixed");
+document.documentElement.style.setProperty('--margintop', 0);
+document.documentElement.style.setProperty('--hovedbannerposition', hovedbannerposition.toString() + "px");
+document.documentElement.style.setProperty('--headerPosition', headerPosition.toString() + "px");
+var prevScrolled = 0;
+
+
+window.addEventListener('scroll', () => {
+	var scrolled = window.scrollY;
+	const difference = scrolled - prevScrolled;
+	if (difference >= 0) {
+		if (headerPosition > -headerHeight) {
+			headerPosition = headerPosition - difference;
+			hovedbannerposition = headerPosition + headerHeight;
+		}
+		else {
+			headerPosition = -headerHeight
+			hovedbannerposition = 0;
+		}
+	}
+	else {
+		if (headerPosition < 0) {
+			headerPosition = headerPosition - difference;
+			hovedbannerposition = headerPosition + headerHeight;
+		}
+		else if (scrolled < headerStartPosition) {
+			headerPosition = headerStartPosition - scrolled;
+			hovedbannerposition = headerPosition + headerHeight;
+		}
+		else {
+			headerPosition = 0;
+			hovedbannerposition = headerHeight;
+		}
+
+	}
+	document.documentElement.style.setProperty('--headerPosition', headerPosition.toString() + "px");
+	document.documentElement.style.setProperty('--hovedbannerposition', hovedbannerposition.toString() + "px");
+ 
+	prevScrolled = scrolled;
+
+});
+
+
+export default class GlobalCommunityHeader extends LightningElement {
+
+	@api NAVarea;
+	@track isPrivatPerson = true;
+	connectedCallback(){
+		//this.isPrivatPerson = this.NAVarea == 'Privatperson';
+		//console.log(this.NAVarea);
+		//console.log(this.isPrivatPerson);
+	}
+
+}
