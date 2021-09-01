00b1ecf59f1314fac6d3f88ba447664d13773320
eirikfladby <eirik.fladby@nav.no>
2021-09-01 13:33:58 +0200

    added custom styling parameters

--- force-app/main/default/lwc/picklist/picklist.js
+++ force-app/main/default/lwc/picklist/picklist.js
@@ -7,11 +7,28 @@ export default class Picklist extends LightningElement {
     @api multiple;
     @api required;
     @api size;
+    @api desktopStyle;
+    @api mobileStyle;
 
     @api choiceValue;
     handleChoiceMade(event) {
         this.choiceValue = event.target.value;
         const eventToSend = new CustomEvent('picklistvaluechange', { detail: this.choiceValue });
         this.dispatchEvent(eventToSend);
+    }
+
+    setDefaultValue(value, valueToSet) {
+        if (value === undefined) {
+            return valueToSet;
+        }
+        return value;
+    }
+
+    get setDefaultStyle() {
+        let style = this.desktopStyle;
+        if (window.screen.width < 576) {
+            style = this.mobileStyle;
+        }
+        return this.setDefaultValue(style, '');
     }
 }
