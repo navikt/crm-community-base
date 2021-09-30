942b0494c71862fd68e3a2960b5f67a9c1b963ae
eirikfladby <eirik.fladby@nav.no>
2021-09-30 14:05:48 +0200

    Added parameter for contentdocuments

--- force-app/main/default/lwc/recordFiles/recordFiles.js
+++ force-app/main/default/lwc/recordFiles/recordFiles.js
@@ -4,15 +4,29 @@ import getBaseDownloadUrl from '@salesforce/apex/RecordFilesController.getBaseDo
 import { setDefaultValue } from 'c/componentHelperClass';
 
 export default class recordFiles extends LightningElement {
-    @api contentDocuments = [];
+    @api recordId;
     @api title;
+    @api contentDocuments = [];
+    contentDocumentsToShow = [];
+    isContentDocumentsEmpty = true;
 
     get defaultTitle() {
         return setDefaultValue(this.title, 'Vedlegg');
     }
 
-    isContentDocumentsEmpty = true;
-    renderedCallback() {
-        this.isContentDocumentsEmpty = this.contentDocuments.length === 0 ? true : false;
+    @wire(getContentDocuments, { recordId: '$recordId' })
+    async wiredgetContentDocuments(result) {
+        if (result.data) {
+            const url = await getBaseDownloadUrl();
+            this.contentDocumentsToShow = result.data.map((item) => ({
+                ...item,
+                downloadLink: url + item.Id
+            }));
+            this.isContentDocumentsEmpty = this.contentDocumentsToShow.length === 0 ? true : false;
+        } else {
+            // User sent content documents via argument?
+            this.contentDocumentsToShow = this.contentDocuments;
+            this.isContentDocumentsEmpty = this.contentDocuments.length === 0 ? true : false;
+        }
     }
 }
