5ae59fb5f809bd0793051b0bc8eb4ac0adb06964
eirikfladby <eirik.fladby@nav.no>
2021-10-06 14:39:25 +0200

    record files component and controller without sharing

---
+++ force-app/main/default/lwc/recordFilesWithoutSharing/recordFilesWithoutSharing.js
@@ -0,0 +1,35 @@
+import { LightningElement, api, wire } from 'lwc';
+import getContentDocuments from '@salesforce/apex/RecordFilesControllerWithoutSharing.getContentDocuments';
+import getBaseDownloadUrl from '@salesforce/apex/RecordFilesControllerWithoutSharing.getBaseDownloadUrl';
+import { setDefaultValue } from 'c/componentHelperClass';
+export default class recordFilesWithoutSharing extends LightningElement {
+    @api recordId;
+    @api title;
+    @api files = [];
+    contentDocuments = [];
+    isContentDocumentsEmpty = false;
+
+    get contentDocumentsArray() {
+        return this.recordId === undefined ? this.files : this.contentDocuments;
+    }
+
+    get defaultTitle() {
+        return setDefaultValue(this.title, 'Vedlegg');
+    }
+
+    renderedCallback() {
+        this.isContentDocumentsEmpty = this.contentDocuments.length === 0 && this.files.length === 0 ? true : false;
+    }
+
+    @wire(getContentDocuments, { recordId: '$recordId' })
+    async wiredgetContentDocuments(result) {
+        if (result.data) {
+            const url = await getBaseDownloadUrl();
+            this.contentDocuments = result.data.map((item) => ({
+                ...item,
+                downloadLink: url + item.Id
+            }));
+            this.isContentDocumentsEmpty = this.contentDocuments.length === 0 && this.files.length === 0 ? true : false;
+        }
+    }
+}
