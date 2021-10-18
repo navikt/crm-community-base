ecb6c9c3a43cebe39fec9b49d335808d1a534def
eirikfladby <eirik.fladby@nav.no>
2021-10-18 13:45:29 +0200

    added functionality for deleting files

--- force-app/main/default/lwc/recordFilesWithoutSharing/recordFilesWithoutSharing.js
+++ force-app/main/default/lwc/recordFilesWithoutSharing/recordFilesWithoutSharing.js
@@ -1,14 +1,19 @@
 import { LightningElement, api, wire } from 'lwc';
 import getContentDocuments from '@salesforce/apex/RecordFilesControllerWithoutSharing.getContentDocuments';
 import getBaseDownloadUrl from '@salesforce/apex/RecordFilesControllerWithoutSharing.getBaseDownloadUrl';
+import deleteFilesOnRecord from '@salesforce/apex/RecordFilesControllerWithoutSharing.deleteFilesOnRecord';
+import getOnlyMyContentDocuments from '@salesforce/apex/RecordFilesControllerWithoutSharing.getOnlyMyContentDocuments';
 import { setDefaultValue } from 'c/componentHelperClass';
-export default class recordFilesWithoutSharing extends LightningElement {
+export default class recordFilesWithSharing extends LightningElement {
     @api recordId;
     @api title;
     @api files = [];
-    @api isGetAll;
+    @api isGetAll = false;
+    @api isDeleteOption = false;
     contentDocuments = [];
+    myContentDocuments = [];
     isContentDocumentsEmpty = false;
+    filesToShow = [];
 
     get contentDocumentsArray() {
         return this.recordId === undefined ? this.files : this.contentDocuments;
@@ -19,7 +24,53 @@ export default class recordFilesWithoutSharing extends LightningElement {
     }
 
     renderedCallback() {
+        this.hasFiles();
+    }
+
+    hasFiles() {
         this.isContentDocumentsEmpty = this.contentDocuments.length === 0 && this.files.length === 0 ? true : false;
+    }
+
+    filesChanged = true; // If true -> shows new files added in list
+    // Make boolean value change and set it to true to show new files added
+    boolSwitch() {
+        this.filesChanged = false;
+        this.filesChanged = true;
+    }
+
+    fileButtonLabel;
+    onFileFocus(event) {
+        this.fileButtonLabel = '';
+        const index = event.currentTarget.dataset.index;
+        this.fileButtonLabel = 'Slett vedlegg ' + this.filesToShow[index].Title;
+    }
+
+    markFilesAvailableForDeletion() {
+        this.filesToShow = [...this.contentDocumentsArray]; // Copy value to be able to set new attribute
+        this.filesToShow.forEach((item) => {
+            this.myContentDocuments.forEach((myItem) => {
+                if (item.Id === myItem) {
+                    item.isDeletable = true;
+                }
+            });
+        });
+    }
+
+    // Call this when submit/save is run to delete selected files
+    // Should probably force a refresh when file is deleted so that the new list of files is shown
+    @api deleteMarkedFiles() {
+        deleteFilesOnRecord({ files: this.filesToDelete });
+    }
+
+    filesToDelete = [];
+    onFileDelete(event) {
+        const index = event.currentTarget.dataset.index;
+        if (this.filesToShow.length < index) {
+            return;
+        }
+        this.filesToDelete.push(this.filesToShow[index].Id);
+        this.filesToShow.splice(index, 1);
+        this.boolSwitch();
     }
 
     @wire(getContentDocuments, { recordId: '$recordId', isGetAll: '$isGetAll' })
@@ -30,7 +81,16 @@ export default class recordFilesWithoutSharing extends LightningElement {
                 ...item,
                 downloadLink: url + item.Id
             }));
-            this.isContentDocumentsEmpty = this.contentDocuments.length === 0 && this.files.length === 0 ? true : false;
+            this.hasFiles();
+            let contentDocumentIds = [];
+            this.contentDocuments.forEach((item) => {
+                contentDocumentIds.push(item.Id);
+            });
+            getOnlyMyContentDocuments({ contentDocumentIds: contentDocumentIds }).then((res) => {
+                this.myContentDocuments = res;
+                this.markFilesAvailableForDeletion();
+            });
         }
     }
 }
+
