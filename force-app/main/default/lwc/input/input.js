3af8f6f50eb311f01f20ad6bd09fe37bbc5585b6
eirikfladby <eirik.fladby@nav.no>
2021-10-19 14:25:45 +0200

    input component init

---
+++ force-app/main/default/lwc/input/input.js
@@ -0,0 +1,71 @@
+import { LightningElement, api } from 'lwc';
+import { setDefaultValue } from 'c/componentHelperClass';
+
+export default class Input extends LightningElement {
+    @api type = 'text';
+    @api name = '';
+    @api alt = ''; // Alternate text for image
+    @api description = '';
+    @api width;
+    @api height;
+    @api autofocus = false;
+    @api disabled = false;
+    @api required = false;
+    @api readonly = false;
+    @api maxLength = '255';
+    @api minLength = '0';
+    @api placeholder = '';
+    @api id = 'inputcomponent';
+    @api mobileStyle;
+    @api desktopStyle;
+
+    get hasDescription() {
+        return this.description !== '' && this.description !== undefined;
+    }
+
+    // Call this when value is needed
+    @api sendValue() {
+        let inputValue = this.template.querySelector('input').value;
+        return inputValue;
+    }
+
+    // Sends value on change
+    sendValueOnChange() {
+        let inputValue = this.template.querySelector('input').value;
+        const selectedEvent = new CustomEvent('getvalueonchange', {
+            detail: inputValue
+        });
+        this.dispatchEvent(selectedEvent);
+    }
+
+    @api
+    validationHandler(errorMessage) {
+        let inputEle = this.template.querySelector('input');
+        inputEle.setCustomValidity(errorMessage);
+        inputEle.reportValidity();
+        if (errorMessage !== '') {
+            inputEle.focus();
+        }
+    }
+
+    get setDefaultStyle() {
+        let style = this.desktopStyle;
+        if (window.screen.width < 576) {
+            style = this.mobileStyle;
+        }
+        return setDefaultValue(style, '');
+    }
+
+    defaultWidth() {
+        return setDefaultValue(this.width, '100%');
+    }
+
+    defaultHeight() {
+        return setDefaultValue(this.height, '48px');
+    }
+
+    renderedCallback() {
+        document.documentElement.style.setProperty('--width', this.defaultWidth());
+        document.documentElement.style.setProperty('--height', this.defaultHeight());
+    }
+}
