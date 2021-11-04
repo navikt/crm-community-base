017bd2cbd1a9c7187f9ba59d9ec394fc73ffb375
eirikfladby <eirik.fladby@nav.no>
2021-11-04 16:02:12 +0100

    added validation to picklist

--- force-app/main/default/lwc/picklist/picklist.js
+++ force-app/main/default/lwc/picklist/picklist.js
@@ -8,13 +8,14 @@ export default class Picklist extends LightningElement {
     @api multiple;
     @api required;
     @api size;
-    @api placeholder;
+    @api errorText;
+    @api placeholderText;
     @api helptext = false;
     @api helptextContent = '';
     @api desktopStyle;
     @api mobileStyle;
 
-    @api choiceValue;
+    choiceValue = '';
     handleChoiceMade(event) {
         for (let choice of this.choices) {
             if (choice.name === event.target.value) {
@@ -36,7 +37,28 @@ export default class Picklist extends LightningElement {
         }
         return setDefaultValue(style, '');
     }
+
     @api getElement() {
         return this.template.querySelector('select');
+    }
+
+    @api
+    validationHandler() {
+        return this.updateShowErrorTextValue();
+    }
+
+    showErrorText = false;
+    updateShowErrorTextValue() {
+        this.showErrorText =
+            this.choiceValue.label === this.placeholderText &&
+            this.errorText !== undefined &&
+            this.errorText !== '' &&
+            !this.disabled;
+        if (this.showErrorText) {
+            this.template.querySelector('.skjemaelement').classList.add('navds-select--error');
+        } else {
+            this.template.querySelector('.skjemaelement').classList.remove('navds-select--error');
+        }
+        return this.showErrorText;
     }
 }
