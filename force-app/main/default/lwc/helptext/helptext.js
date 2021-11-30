df0498a39d98f4521a1bb9dfa566da41e27c0e0e
vmjelde <vegard.mjelde@soprasteria.com>
2021-11-30 11:49:16 +0100

    helptext position styling
    https://jira.adeo.no/browse/TOLK-1777

--- force-app/main/default/lwc/helptext/helptext.js
+++ force-app/main/default/lwc/helptext/helptext.js
@@ -12,6 +12,27 @@ export default class Helptext extends LightningElement {
         if (window.screen.width < 576) {
             style = this.mobileStyle;
         }
-        return setDefaultValue(style, 'white-space: normal; width: 40vw');
+        return setDefaultValue(style, '');
     }
+    setHelptextPostition() {
+        if (window.screen.width < 576) {
+            let element = this.template.querySelector('.navds-help-text__button');
+            let rect = element.getBoundingClientRect();
+            console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+            let left = 3 - rect.left;
+            let right = 3 + rect.right - window.screen.width;
+            let arrowRight = window.screen.width - rect.right;
+            let moveToRight = window.screen.width * 0.55 < rect.left;
+            //this.arrowStyle = 'left: ' + rect.left + 'px;';
+            this.defaultStyle = 'left: ' + left + 'px;';
+            this.arrowStyle = 'left: ' + rect.left + 'px;';
+            if (moveToRight) {
+                this.defaultStyle = 'left: auto; right: ' + right + 'px;';
+                this.arrowStyle = 'left: auto; right: ' + arrowRight + 'px;';
+            }
+        }
+    }
+    defaultStyle = '';
+    arrowStyle = '';
 }
