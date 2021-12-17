1a6e09dcaed8c1b284b6254b4b11e93599fbb42d
eirikfladby <eirik.fladby@nav.no>
2021-12-17 09:57:18 +0100

    check disabled

--- force-app/main/default/lwc/input/input.js
+++ force-app/main/default/lwc/input/input.js
@@ -150,8 +150,11 @@ export default class Input extends LightningElement {
         let number = this.template.querySelector('input').value.replaceAll(' ', '');
         this.showError = number.length < 8 ? true : false;
         this.actualErrorText = errMsg;
-        this.setErrorCss();
-        return this.showError;
+        if (!this.disabled) {
+            this.setErrorCss();
+            return this.showError;
+        }
+        return false;
     }
 
     @api
