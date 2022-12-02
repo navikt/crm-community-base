326a47c6e4df951fbbe6d106cb90652688551f55
Morten Mikalsen <morten.mikalsen@hotmail.com>
2022-12-02 12:18:35 +0100

    Clean up code

--- force-app/components/lwc/salesforceFooter/salesforceFooter.js
+++ force-app/components/lwc/salesforceFooter/salesforceFooter.js
@@ -2,11 +2,4 @@ import { LightningElement } from 'lwc';
 
 export default class SalesforceFooter extends LightningElement {
     static renderMode = 'light'; // the default is 'shadow'
-
-    // renderedCallback() {
-    //     const troll = document.createElement('div');
-    //     troll.className = 'decorator-footer';
-    //     let div = this.template.querySelector('.insElement');
-    //     div.appendChild(troll);
-    // }
 }
