6c5c8cfd3a93509230f832931c2693cb6ab792f0
annolse <ann.kristin.olsen@nav.no>
2022-12-07 09:43:45 +0100

    Added aria expanded  functionality

--- force-app/main/default/lwc/collapsableSection/collapsableSection.js
+++ force-app/main/default/lwc/collapsableSection/collapsableSection.js
@@ -1,10 +1,12 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class CollapsableSection extends LightningElement {
     @api id = 1;
     @api label;
+    @track ariaExpanded = false;
 
     toggleSection(event){
+        this.ariaExpanded = !this.ariaExpanded;
         let buttonid = event.currentTarget.dataset.buttonid;
         let currentsection = this.template.querySelector('[data-id="'+buttonid + '"]');
         if(currentsection.className.search('slds-is-open')==-1){
